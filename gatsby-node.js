249d15d272a5e23cd2aa16f71af034a4233010f4
Luca Gandini <luca.gandini88@gmail.com>
2020-11-24 18:30:45 +0100

    Remote API project settings

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
 // You can delete this file if you're not using it
+const axios = require("axios").default
+
+// constants for your GraphQL Post and Author types
+const POST_NODE_TYPE = `Team`
+
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+  getNodesByType,
+}) => {
+  const { createNode } = actions
+
+  const data = {};
+
+  const fetchTeams = async () =>
+    axios.get("https://api.football-data.org/v2/competitions/SA/teams", {
+      headers: { "X-Auth-Token": process.env.API_TOKEN },
+    });
+
+  data.teams = (await fetchTeams()).data.teams;
+
+  // loop through data and create Gatsby nodes
+  data.teams.forEach(team =>
+    createNode({
+      ...team,
+      id: createNodeId(`${POST_NODE_TYPE}-${team.id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: POST_NODE_TYPE,
+        content: JSON.stringify(team),
+        contentDigest: createContentDigest(team),
+      },
+    })
+  )
+
+  return;
+}
