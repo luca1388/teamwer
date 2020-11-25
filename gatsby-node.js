651959e77eba3863014aa1b2f344cf41e8df0e21
Luca Gandini <luca.gandini88@gmail.com>
2020-11-25 21:41:28 +0100

    Fetching table data at build time

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,6 +13,7 @@ require("dotenv").config({
 
 // constants for your GraphQL Post and Author types
 const POST_NODE_TYPE = `Team`
+const TABLE_POSITION_NODE_TYPE = `Position`
 
 exports.sourceNodes = async ({
   actions,
@@ -28,8 +29,13 @@ exports.sourceNodes = async ({
     axios.get("https://api.football-data.org/v2/competitions/SA/teams", {
       headers: { "X-Auth-Token": process.env.API_TOKEN },
     })
+  const fetchTable = async () =>
+    axios.get("https://api.football-data.org/v2/competitions/SA/standings", {
+      headers: { "X-Auth-Token": process.env.API_TOKEN },
+    })
 
   data.teams = (await fetchTeams()).data.teams
+  data.table = (await fetchTable()).data.standings[0].table;
 
   // loop through data and create Gatsby nodes
   data.teams.forEach(team =>
@@ -45,6 +51,35 @@ exports.sourceNodes = async ({
       },
     })
   )
+  createNode({
+    ...data.table,
+    id: createNodeId(`${TABLE_POSITION_NODE_TYPE}-`),
+    parent: null,
+    children: [],
+    internal: {
+      type: TABLE_POSITION_NODE_TYPE,
+      content: JSON.stringify(data.table),
+      contentDigest: createContentDigest(data.table),
+    },
+  })
+
+  // data.table.forEach(position =>
+  //   createNode({
+  //     ...position,
+  //     id: createNodeId(`${TABLE_POSITION_NODE_TYPE}-${position.position}`),
+  //     parent: null,
+  //     children: [],
+  //     internal: {
+  //       type: TABLE_POSITION_NODE_TYPE,
+  //       content: JSON.stringify(position),
+  //       contentDigest: createContentDigest(position),
+  //     },
+  //   })
+  // )
 
   return
+}
+
+exports.onCreateNode = ({ node }) => {
+  console.log(`Node created of type "${node.internal.type}"`)
 }
