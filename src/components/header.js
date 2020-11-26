8e609cd4280464cffb689dd93fee04d3828fab15
Luca Gandini <luca.gandini88@gmail.com>
2020-11-26 19:33:27 +0100

    Fixed Header style

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,16 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#2f2f2f`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        width: "80%",
         padding: `1.45rem 1.0875rem`,
+        display: "flex",
       }}
     >
       <h1 style={{ margin: 0 }}>
