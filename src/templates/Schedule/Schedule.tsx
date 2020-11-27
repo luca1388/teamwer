ca5fc520b660b23cc3cd591b4c6ecbb058f4e23a
Luca Gandini <luca.gandini88@gmail.com>
2020-11-27 18:08:05 +0100

    Improved a bit look & feel of schedule page

--- src/templates/Schedule/Schedule.tsx
+++ src/templates/Schedule/Schedule.tsx
@@ -1,27 +1,31 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../../components/layout"
-import SEO from "../../components/seo"
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../../components/layout";
+import SEO from "../../components/seo";
+
+import "./Schedule.css";
 
 const Schedule = ({ data, pageContext }) => {
   const matches = data.allMatch.edges.filter(
     edge => edge.node.teamId === pageContext.teamId
-  )
+  );
 
   return (
     <Layout>
       <SEO title={"Calendario " + pageContext.teamName}></SEO>
       {matches.map(match => (
-        <div key={match.node.id}>
-          <div>{match.node.homeTeam.name}</div>
-          <div>{match.node.awayTeam.name}</div>
+        <div className="schedule-container" key={match.node.id}>
+          <div className="schedule-team">{match.node.homeTeam.name}</div>
+          <div className="schedule-score">{match.node.score.fullTime.homeTeam}</div>-
+          <div className="schedule-score">{match.node.score.fullTime.awayTeam}</div>
+          <div className="schedule-team">{match.node.awayTeam.name}</div>
         </div>
       ))}
     </Layout>
-  )
-}
+  );
+};
 
-export default Schedule
+export default Schedule;
 
 export const query = graphql`
   query {
@@ -48,4 +52,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
