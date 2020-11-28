e1a660095cc99b6bb984cb5cc9ed54af8f79c915
Luca Gandini <luca.gandini88@gmail.com>
2020-11-28 12:15:29 +0100

    Improved SEO component with image field

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `Teamwer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Check out the results of your favourite Serie A Italian soccer team!`,
+    author: `@luca1388`,
+    image: ''
   },
   plugins: [
     {
