4bc7e221a1ad038575df2432864d35c5b1b22294
Luca Gandini <luca.gandini88@gmail.com>
2020-11-28 14:03:46 +0100

    Fixed Links type

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -4,20 +4,21 @@ import React from "react";
 import "./Toolbar.css";
 
 interface ToolbarProps {
-  links: [
-    {
-      to: string
-      label: string
-    }
-  ]
+  links: {
+    to: string;
+    label: string;
+  }[];
 }
 
 const Toolbar: React.FC<ToolbarProps> = ({ links }) => (
   <div className="toolbar">
     {links.map((link, index) => (
-      <span key={link.to}><Link to={link.to}>{link.label}</Link> {index !== (links.length - 1) && "|"} </span>
+      <span key={link.to}>
+        <Link to={link.to}>{link.label}</Link>{" "}
+        {index !== links.length - 1 && "|"}{" "}
+      </span>
     ))}
   </div>
-)
+);
 
 export default Toolbar;
