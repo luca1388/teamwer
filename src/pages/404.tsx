b3d80005df400fc98fec7fe4649745a5374b6f42
Luca Gandini <luca.gandini88@gmail.com>
2020-12-01 18:58:46 +0100

    Refactored 404 page with Typescript

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 import SEO from "../components/SEO/SEO";
 
-const NotFoundPage = () => (
+const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
