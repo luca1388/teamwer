98c1b4c0a2fe31cf8b9637b691ee1a6e159d467b
Luca Gandini <luca.gandini88@gmail.com>
2020-12-03 16:32:53 +0100

    Fixed Header style

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -17,6 +17,8 @@ const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
         width: "80%",
         padding: `1.45rem 1.0875rem`,
         display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center"
       }}
     >
       <h1 style={{ margin: 0 }}>
